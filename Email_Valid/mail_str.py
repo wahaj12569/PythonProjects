012feec2bddb65ec7d8ffc228405ebcc56fd7e0a
wahaj12569 <wahaj12569@gmail.com>
2024-08-26 11:01:21 +0500

    8/26/2024 11:01AM

--- Email_Valid/mail_str.py
+++ Email_Valid/mail_str.py
@@ -1,7 +1,27 @@
 email = input("Enter your Email:")
+k,j,d=0,0,0
 if len(email) >= 6:
     if email[0].isalpha():
-        pass
+        if ("@" in email)and (email.count("@")==1):
+            if (email[-4]==".") ^ (email[-3]=="."):
+                for i in email:
+                    if i==i.isspace():
+                        k=1
+                    elif i.isalpha():
+                        if i==i.upper():
+                            j=1
+                    elif i.isdigit():
+                            continue
+                    elif i =="_" or i=="." or i=="@":
+                        continue
+                    else:
+                        d=1
+                if (k==1 or j==1 or d==1):
+                    print("worng email 5")
+            else:
+                print ("worng email 4")
+        else:
+            print("worng email 3")
     else:
         print('wrong email 2')
 else:
