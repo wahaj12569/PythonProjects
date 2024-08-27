ca7e786e120189fdc133df35c6ef4765c8f8a5b9
wahaj12569 <wahaj12569@gmail.com>
2024-08-27 14:17:54 +0500

    27/8/2024 2:18PM

--- Instagram_Bot/Automation.py
+++ Instagram_Bot/Automation.py
@@ -1,5 +1,12 @@
 from instabot import Bot
 bot=Bot()
-bot.login(username="migi_and_dali9",password="lthaaw12569")
-bot.follow("wscubetechindia")
-# bot.upload_photo("/Users/mini/Downloads/Amazon-Logo-31.png",caption="congratulations!")
+# bot.login(username="migi_and_dali9",password="lthaaw12569")
+bot.login(username="itach_i5747",password="jerry123")
+# bot.follow("wscubetechindia")
+# bot.upload_photo("/Users/mini/Downloads/Amazon-Logo-31.png",caption="congratulations!") 
+# bot.unfollow("wscubetechindia")
+# bot.send_message("kasa ha? nafay!",['wahajlatif_official'])
+# followers =bot.get_user_followers("itach_i5747")
+# for follower in followers:
+#     print(bot.get_user_info(follower))
+
